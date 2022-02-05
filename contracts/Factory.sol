8c0582d3ef10697000318aee5d63ffbd8869c1aa
Suchan An <dobestan@gmail.com>
2022-02-05 14:50:59 +0900

    Update Factory contract: createToken feature

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -2,10 +2,23 @@
 pragma solidity ^0.8.0;
 
 
+import "./Token.sol";
+
+
 contract Factory {
     address public owner;
 
     constructor() {
         owner = msg.sender;
+    }
+
+    function createToken(
+        string memory name_,
+        string memory symbol_,
+        uint initial_
+    ) public returns (address) {
+        Token token = new Token(name_, symbol_, initial_);
+        return address(token);
+        // token.receipt.rawLogs[0].address is ERC-20 Contract Address.
     }
 }
--- test/factory.test.js
+++ test/factory.test.js
@@ -1,12 +1,42 @@
 const Factory = artifacts.require("Factory");
+const Token = artifacts.require("Token");
 
 
 contract("Factory", (accounts) => {
+    let factory;
+
+    before("get deployed factory contract.", async () => {
+        factory = await Factory.deployed();
+    });
+
     it("should have a owner.", async () => {
-        const factory = await Factory.deployed();
         assert.equal(
             await factory.owner(),
             accounts[0],
         );
+    });
+
+    describe("createToken function", () => {
+        it("should create a token contract.", async () => {
+            const name_ = "UltimatePowerfulBitcoin";
+            const symbol_ = "UPBIT";
+            const initial_ = 1000000;
+
+            const deployed = await factory.createToken(
+                name_,
+                symbol_,
+                initial_
+            );
+            const deployedTokenAddress = deployed.receipt.rawLogs[0].address;
+            const deployedToken = await Token.at(deployedTokenAddress);
+
+            assert.ok(deployedToken);
+            assert.equal(await deployedToken.name(), name_);
+            assert.equal(await deployedToken.symbol(), symbol_);
+            assert.equal(
+                web3.utils.fromWei(await deployedToken.balanceOf(factory.address), "ether"),
+                initial_
+            )
+        });
     });
 });
