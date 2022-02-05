6377a9ffd66c09d852703995d4fdfd4007ee6605
Suchan An <dobestan@gmail.com>
2022-02-05 20:48:50 +0900

    Add createToken feature

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,11 @@ import './App.css';
 const DEPLOYED_FACTORY_CONTRACT_ADDRESS = "0x75ea6bca6d9b6e26368ff6a28cf0a74b6c338e7d";
 
 
+let factory;
+
+
 class App extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -22,6 +26,10 @@ class App extends Component {
             factory: DEPLOYED_FACTORY_CONTRACT_ADDRESS,
             account: [],
             balance: 0,
+
+            name: "",
+            symbol: "",
+            initial: 0,
         };
     }
 
@@ -31,7 +39,7 @@ class App extends Component {
             const accounts = await web3.eth.getAccounts();
             const account = accounts[0];
 
-            const factory = new web3.eth.Contract(
+            factory = new web3.eth.Contract(
                 FactoryContract.abi,
                 DEPLOYED_FACTORY_CONTRACT_ADDRESS
             );
@@ -49,6 +57,18 @@ class App extends Component {
         }
     }
 
+    createToken = (event) => {
+        event.preventDefault();
+
+        const name = event.target.name.value;
+        const symbol = event.target.symbol.value;
+        const initial = event.target.initial.value;
+
+        factory.methods.createToken(name, symbol, initial).send({from: this.state.account});
+        // #TODO: this method is not tested due to high tx fee on mainnet.
+        // should change to testnet for development.
+    }
+
     render() {
         return (
             <div>
@@ -56,6 +76,13 @@ class App extends Component {
                 <p>{this.state.factory}</p>
 
                 <AccountDetail account={this.state.account} balance={this.state.balance} />
+                <h1>Create Token Form</h1>
+                <form onSubmit={this.createToken}>
+                    <input name="name" type="text" placeholder="Token Name" required />
+                    <input name="symbol" type="text" placeholder="Token Symbol" required />
+                    <input name="initial" type="number" placeholder="Initial Tokens" required />
+                    <input type="submit" />
+                </form>
                 <TokenList tokens={this.state.tokens} />
             </div>
         );
